e54d523f6ab3f04955740c89c327a538fa2e793e
DAZ <daz4126@gmail.com>
2018-02-14 16:21:02 +0000

    Changed to list app

---
+++ docs/730366ab6056315633501f5ca3338941.js
@@ -0,0 +1,683 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+require = (function (modules, cache, entry) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof require === "function" && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof require === "function" && require;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+      
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module;
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module() {
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({16:[function(require,module,exports) {
+var bundleURL = null;
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error;
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^)\n]+/g);
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+
+},{}],14:[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+  newLink.onload = function () {
+    link.remove();
+  };
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+
+},{"./bundle-url":16}],6:[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"_css_loader":14}],15:[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.h = h;
+exports.app = app;
+function h(name, attributes /*, ...rest*/) {
+  var node;
+  var rest = [];
+  var children = [];
+  var length = arguments.length;
+
+  while (length-- > 2) {
+    rest.push(arguments[length]);
+  }while (rest.length) {
+    if ((node = rest.pop()) && node.pop /* Array? */) {
+        for (length = node.length; length--;) {
+          rest.push(node[length]);
+        }
+      } else if (node != null && node !== true && node !== false) {
+      children.push(node);
+    }
+  }
+
+  return typeof name === "function" ? name(attributes || {}, children) : {
+    nodeName: name,
+    attributes: attributes || {},
+    children: children,
+    key: attributes && attributes.key
+  };
+}
+
+function app(state, actions, view, container) {
+  var renderLock;
+  var invokeLaterStack = [];
+  var rootElement = container && container.children[0] || null;
+  var oldNode = rootElement && toVNode(rootElement, [].map);
+  var globalState = clone(state);
+  var wiredActions = clone(actions);
+
+  scheduleRender(wireStateToActions([], globalState, wiredActions));
+
+  return wiredActions;
+
+  function toVNode(element, map) {
+    return {
+      nodeName: element.nodeName.toLowerCase(),
+      attributes: {},
+      children: map.call(element.childNodes, function (element) {
+        return element.nodeType === 3 // Node.TEXT_NODE
+        ? element.nodeValue : toVNode(element, map);
+      })
+    };
+  }
+
+  function render() {
+    renderLock = !renderLock;
+
+    var next = view(globalState, wiredActions);
+    if (container && !renderLock) {
+      rootElement = patch(container, rootElement, oldNode, oldNode = next);
+    }
+
+    while (next = invokeLaterStack.pop()) {
+      next();
+    }
+  }
+
+  function scheduleRender() {
+    if (!renderLock) {
+      renderLock = !renderLock;
+      setTimeout(render);
+    }
+  }
+
+  function clone(target, source) {
+    var obj = {};
+
+    for (var i in target) {
+      obj[i] = target[i];
+    }for (var i in source) {
+      obj[i] = source[i];
+    }return obj;
+  }
+
+  function set(path, value, source) {
+    var target = {};
+    if (path.length) {
+      target[path[0]] = path.length > 1 ? set(path.slice(1), value, source[path[0]]) : value;
+      return clone(source, target);
+    }
+    return value;
+  }
+
+  function get(path, source) {
+    for (var i = 0; i < path.length; i++) {
+      source = source[path[i]];
+    }
+    return source;
+  }
+
+  function wireStateToActions(path, state, actions) {
+    for (var key in actions) {
+      typeof actions[key] === "function" ? function (key, action) {
+        actions[key] = function (data) {
+          if (typeof (data = action(data)) === "function") {
+            data = data(get(path, globalState), actions);
+          }
+
+          if (data && data !== (state = get(path, globalState)) && !data.then // Promise
+          ) {
+              scheduleRender(globalState = set(path, clone(state, data), globalState));
+            }
+
+          return data;
+        };
+      }(key, actions[key]) : wireStateToActions(path.concat(key), state[key] = state[key] || {}, actions[key] = clone(actions[key]));
+    }
+  }
+
+  function getKey(node) {
+    return node ? node.key : null;
+  }
+
+  function setElementProp(element, name, value, isSVG, oldValue) {
+    if (name === "key") {} else if (name === "style") {
+      for (var i in clone(oldValue, value)) {
+        element[name][i] = value == null || value[i] == null ? "" : value[i];
+      }
+    } else {
+      if (typeof value === "function" || name in element && !isSVG) {
+        element[name] = value == null ? "" : value;
+      } else if (value != null && value !== false) {
+        element.setAttribute(name, value);
+      }
+
+      if (value == null || value === false) {
+        element.removeAttribute(name);
+      }
+    }
+  }
+
+  function createElement(node, isSVG) {
+    var element = typeof node === "string" || typeof node === "number" ? document.createTextNode(node) : (isSVG = isSVG || node.nodeName === "svg") ? document.createElementNS("http://www.w3.org/2000/svg", node.nodeName) : document.createElement(node.nodeName);
+
+    if (node.attributes) {
+      if (node.attributes.oncreate) {
+        invokeLaterStack.push(function () {
+          node.attributes.oncreate(element);
+        });
+      }
+
+      for (var i = 0; i < node.children.length; i++) {
+        element.appendChild(createElement(node.children[i], isSVG));
+      }
+
+      for (var name in node.attributes) {
+        setElementProp(element, name, node.attributes[name], isSVG);
+      }
+    }
+
+    return element;
+  }
+
+  function updateElement(element, oldProps, attributes, isSVG) {
+    for (var name in clone(oldProps, attributes)) {
+      if (attributes[name] !== (name === "value" || name === "checked" ? element[name] : oldProps[name])) {
+        setElementProp(element, name, attributes[name], isSVG, oldProps[name]);
+      }
+    }
+
+    if (attributes.onupdate) {
+      invokeLaterStack.push(function () {
+        attributes.onupdate(element, oldProps);
+      });
+    }
+  }
+
+  function removeChildren(element, node, attributes) {
+    if (attributes = node.attributes) {
+      for (var i = 0; i < node.children.length; i++) {
+        removeChildren(element.childNodes[i], node.children[i]);
+      }
+
+      if (attributes.ondestroy) {
+        attributes.ondestroy(element);
+      }
+    }
+    return element;
+  }
+
+  function removeElement(parent, element, node, cb) {
+    function done() {
+      parent.removeChild(removeChildren(element, node));
+    }
+
+    if (node.attributes && (cb = node.attributes.onremove)) {
+      cb(element, done);
+    } else {
+      done();
+    }
+  }
+
+  function patch(parent, element, oldNode, node, isSVG, nextSibling) {
+    if (node === oldNode) {} else if (oldNode == null) {
+      element = parent.insertBefore(createElement(node, isSVG), element);
+    } else if (node.nodeName && node.nodeName === oldNode.nodeName) {
+      updateElement(element, oldNode.attributes, node.attributes, isSVG = isSVG || node.nodeName === "svg");
+
+      var oldElements = [];
+      var oldKeyed = {};
+      var newKeyed = {};
+
+      for (var i = 0; i < oldNode.children.length; i++) {
+        oldElements[i] = element.childNodes[i];
+
+        var oldChild = oldNode.children[i];
+        var oldKey = getKey(oldChild);
+
+        if (null != oldKey) {
+          oldKeyed[oldKey] = [oldElements[i], oldChild];
+        }
+      }
+
+      var i = 0;
+      var j = 0;
+
+      while (j < node.children.length) {
+        var oldChild = oldNode.children[i];
+        var newChild = node.children[j];
+
+        var oldKey = getKey(oldChild);
+        var newKey = getKey(newChild);
+
+        if (newKeyed[oldKey]) {
+          i++;
+          continue;
+        }
+
+        if (newKey == null) {
+          if (oldKey == null) {
+            patch(element, oldElements[i], oldChild, newChild, isSVG);
+            j++;
+          }
+          i++;
+        } else {
+          var recyledNode = oldKeyed[newKey] || [];
+
+          if (oldKey === newKey) {
+            patch(element, recyledNode[0], recyledNode[1], newChild, isSVG);
+            i++;
+          } else if (recyledNode[0]) {
+            patch(element, element.insertBefore(recyledNode[0], oldElements[i]), recyledNode[1], newChild, isSVG);
+          } else {
+            patch(element, oldElements[i], null, newChild, isSVG);
+          }
+
+          j++;
+          newKeyed[newKey] = newChild;
+        }
+      }
+
+      while (i < oldNode.children.length) {
+        var oldChild = oldNode.children[i];
+        if (getKey(oldChild) == null) {
+          removeElement(element, oldElements[i], oldChild);
+        }
+        i++;
+      }
+
+      for (var i in oldKeyed) {
+        if (!newKeyed[oldKeyed[i][1].key]) {
+          removeElement(element, oldKeyed[i][0], oldKeyed[i][1]);
+        }
+      }
+    } else if (node.nodeName === oldNode.nodeName) {
+      element.nodeValue = node;
+    } else {
+      element = parent.insertBefore(createElement(node, isSVG), nextSibling = element);
+      removeElement(parent, nextSibling, oldNode);
+    }
+    return element;
+  }
+}
+},{}],7:[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var actions = {
+  add: function add() {
+    return function (state) {
+      return {
+        input: '',
+        todos: state.todos.concat({
+          value: state.input,
+          id: state.todos.length + 1 })
+      };
+    };
+  },
+  input: function input(_ref) {
+    var value = _ref.value;
+    return { input: value };
+  }
+};
+
+exports.default = actions;
+},{}],8:[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var state = {
+  todos: [],
+  input: '',
+  placeholder: 'Make a list...'
+};
+
+exports.default = state;
+},{}],21:[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ListItem = undefined;
+
+var _hyperapp = require('hyperapp');
+
+var ListItem = exports.ListItem = function ListItem(_ref) {
+  var value = _ref.value,
+      id = _ref.id;
+  return (0, _hyperapp.h)(
+    'li',
+    { key: id },
+    value
+  );
+};
+},{"hyperapp":15}],9:[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _hyperapp = require('hyperapp');
+
+var _components = require('./components.js');
+
+var view = function view(state, actions) {
+  return (0, _hyperapp.h)(
+    'div',
+    null,
+    (0, _hyperapp.h)(
+      'h1',
+      null,
+      'HyperList'
+    ),
+    (0, _hyperapp.h)(
+      'div',
+      { 'class': 'flex' },
+      (0, _hyperapp.h)('input', {
+        type: 'text'
+        // adds to do when enter is pressed
+        , onkeyup: function onkeyup(e) {
+          return e.keyCode === 13 ? actions.add() : null;
+        }
+        // udpates what is shown in the box
+        , oninput: function oninput(e) {
+          return actions.input({ value: e.target.value });
+        },
+        value: state.input,
+        placeholder: state.placeholder
+      }),
+      (0, _hyperapp.h)(
+        'button',
+        { onclick: actions.add },
+        '\uFF0B'
+      )
+    ),
+    (0, _hyperapp.h)(
+      'ul',
+      null,
+      state.todos.map(function (t) {
+        return (0, _hyperapp.h)(_components.ListItem, {
+          id: t.id,
+          value: t.value
+        });
+      })
+    )
+  );
+};
+
+exports.default = view;
+},{"hyperapp":15,"./components.js":21}],4:[function(require,module,exports) {
+'use strict';
+
+require('./css/index.scss');
+
+var _hyperapp = require('hyperapp');
+
+var _actions = require('./js/actions.js');
+
+var _actions2 = _interopRequireDefault(_actions);
+
+var _state = require('./js/state.js');
+
+var _state2 = _interopRequireDefault(_state);
+
+var _view = require('./js/view.js');
+
+var _view2 = _interopRequireDefault(_view);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var main = (0, _hyperapp.app)(_state2.default, _actions2.default, _view2.default, document.body);
+},{"./css/index.scss":6,"hyperapp":15,"./js/actions.js":7,"./js/state.js":8,"./js/view.js":9}],24:[function(require,module,exports) {
+
+var global = (1, eval)('this');
+var OldModule = module.bundle.Module;
+function Module() {
+  OldModule.call(this);
+  this.hot = {
+    accept: function (fn) {
+      this._acceptCallback = fn || function () {};
+    },
+    dispose: function (fn) {
+      this._disposeCallback = fn;
+    }
+  };
+}
+
+module.bundle.Module = Module;
+
+if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var ws = new WebSocket('ws://' + hostname + ':' + '60450' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      data.assets.forEach(function (asset) {
+        hmrApply(global.require, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.require, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + 'data.error.stack');
+    }
+  };
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  if (cached && cached.hot._disposeCallback) {
+    cached.hot._disposeCallback();
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallback) {
+    cached.hot._acceptCallback();
+    return true;
+  }
+
+  return getParents(global.require, id).some(function (id) {
+    return hmrAccept(global.require, id);
+  });
+}
+},{}]},{},[24,4])
+//# sourceMappingURL=/docs/730366ab6056315633501f5ca3338941.map
---
+++ src/js/components.js
@@ -0,0 +1,3 @@
+import { h } from 'hyperapp'
+
+export const ListItem = ({ value,id }) => <li key={id}>{value}</li>
